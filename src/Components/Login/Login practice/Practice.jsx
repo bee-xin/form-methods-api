09e872669e35c028f4dc99d85bc023d3b0c99adb
unknown <bijen.ito@gmail.com>
2025-02-11 11:58:43 +0545

    create a form using post method

--- src/Components/Login/Login practice/Practice.jsx
+++ src/Components/Login/Login practice/Practice.jsx
@@ -1,47 +1,84 @@
 import { Button, Form, Input } from "antd";
+import axios from "axios";
 import { Link } from "react-router";
-import { FcGoogle } from "react-icons/fc";
 
 function Practice() {
-  return (
-    <div className="flex flex-row gap-10 max-w-[820px] mx-auto mt-[60px] h-[400px] border border-black">
-      <div className=" h-auto w-auto ml-">
-        <img
-          src="wall.jpg"
-          alt="pic"
-          className="h-[400px] w-[450px] rounded-2xl"
-        />
-      </div>
+  const post = async () => {
+    try {
+      const response = await axios.post(
+        "https://fakestoreapi.com/products",
+        {
+          title: "values.fname",
+          category: "values.lname",
+          description: "values.password",
+          image: " values.email",
+        },
+        {
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-      <div>
-        <div className="text-4xl mt-16">Create an Account</div>
+  // const button = (values) => {
+  //   console.log(values);
+  //   post(values);
+  // };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-50">
+      <div className="flex bg-white rounded-xl shadow-lg overflow-hidden w-[750px]">
+        {/* Image Section */}
         <div>
-          Already have an account? <Link>Login</Link>
-        </div>
-        <div className="flex flex-row gap-2 mt-4 ">
-          <Form.Item name="fname" className="w-auto">
-            <Input
-              className="w-[2px]"
-              style={{ width: "144px" }}
-              placeholder="First name"
-            />
-          </Form.Item>
-          <Form.Item name="lname">
-            <Input style={{ width: "145px" }} placeholder="Last name" />
-          </Form.Item>
-        </div>
-        <Form.Item>
-          <Input style={{ width: "300px" }} placeholder="Email" />
-        </Form.Item>
-        <Form.Item>
-          <Input.Password
-            style={{ width: "300px" }}
-            placeholder="Enter your password"
+          <img
+            src="wall.jpg"
+            alt="Login"
+            className="w-[350px] h-full object-cover"
           />
-        </Form.Item>
-        <Button className="w-[300px] p-2" type="primary">
-          Create Account
-        </Button>
+        </div>
+
+        {/* Login Form Section */}
+        <div className="p-8 w-full">
+          <h2 className="text-3xl font-bold text-gray-800 mb-2">
+            Create an Account
+          </h2>
+          <p className="text-gray-600 mb-6">
+            Already have an account?
+            <Link className="text-blue-500 hover:underline">Login</Link>
+          </p>
+
+          <Form>
+            <div className="flex gap-4">
+              <Form.Item name="fname" label="First Name" className="w-1/2">
+                <Input placeholder="First name" />
+              </Form.Item>
+              <Form.Item name="lname" label="Last Name" className="w-1/2">
+                <Input placeholder="Last name" />
+              </Form.Item>
+            </div>
+
+            <Form.Item name="email" label="Email">
+              <Input placeholder="Email" />
+            </Form.Item>
+
+            <Form.Item name="password" label="Password">
+              <Input.Password placeholder="Password" />
+            </Form.Item>
+
+            <Button
+              className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 mt-4"
+              type="primary"
+              onClick={post}
+            >
+              Create Account
+            </Button>
+          </Form>
+        </div>
       </div>
     </div>
   );
